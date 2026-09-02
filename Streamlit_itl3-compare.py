973ef1c5c75723839e81ffae31a5f1dae7fa53a8
McKeoghN <mckeoghn@gmail.com>
2026-09-02 10:27:51 +0100

    Removed low skills table from ITL3 compare tool due to incorrect data

--- Streamlit_itl3-compare.py
+++ Streamlit_itl3-compare.py
@@ -236,7 +236,7 @@ async def main():
     
     carousel_items = ""
     # Convert Plotly bar charts to HTML
-    for i, indicator in enumerate(list(indicators)[1:]):
+    for i, indicator in enumerate([ind for ind in list(indicators)[1:] if ind != 'Low Skilled']):
         bar = visualisations.bar(all_data, indicator, [selected_itl3_1, selected_itl3_2], driver[indicator][0])
         bar = bar.to_html(full_html=False, include_plotlyjs='cdn')
         # Set the first item as active
